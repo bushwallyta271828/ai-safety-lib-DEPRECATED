93d804ea1dd4bc7ff8bd2dd8407210a0ebd059f8
bushwallyta271828 <aheckett@gmail.com>
2024-12-30 15:38:49 -0500

    Defined what null events are in Savage.lean

--- AiSafetyLib/DecisionTheory/Savage.lean
+++ AiSafetyLib/DecisionTheory/Savage.lean
@@ -28,3 +28,11 @@ def sureThingPrinciple {States Consequences : Type}
       notBetterThan (event.piecewise f h) (event.piecewise g h) →
         (conditionalNotBetterThan notBetterThan event) f g
 
+def null {States Consequences : Type}
+  [Nonempty States] [Nonempty Consequences]
+  (notBetterThan : (States → Consequences) → (States → Consequences) → Prop)
+  (event : Set States)
+  : Prop :=
+    ∀ f g : States → Consequences,
+      (conditionalNotBetterThan notBetterThan event) f g
+
