cc8743ed0c01aa86e1b5b30a3fe29c7934328f9e
bushwallyta271828 <aheckett@gmail.com>
2024-12-30 14:41:07 -0500

    Worked on Savage.lean

--- AiSafetyLib/DecisionTheory/Savage.lean
+++ AiSafetyLib/DecisionTheory/Savage.lean
@@ -1,9 +1,14 @@
 import Mathlib
 set_option autoImplicit false
 
-def P1 := sorry
+def completeTransitive {States Consequences : Type}
+  [Nonempty States] [Nonempty Consequences]
+  (notBetterThan : (States → Consequences) → (States → Consequences) → Prop)
+  : Prop :=
+    (∀ f g : States → Consequences, notBetterThan f g ∨ notBetterThan g f)
+    ∧ (∀ f g h : States → Consequences, notBetterThan f g → notBetterThan g h → notBetterThan f h)
 
 def sureThingPrinciple {States Consequences : Type}
-  [nonempty States] [nonempty Consequences]
-  (weaklyPrefer : (States → Consequences) → (States → Consequences) → Prop)
+  [Nonempty States] [Nonempty Consequences]
+  (notBetterThan : (States → Consequences) → (States → Consequences) → Prop)
   : Prop := sorry
