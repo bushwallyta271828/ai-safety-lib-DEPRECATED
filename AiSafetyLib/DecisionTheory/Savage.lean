fcefb493cda565da4b11532a0debe46284aaed63
bushwallyta271828 <aheckett@gmail.com>
2024-12-30 15:26:00 -0500

    Added conditionalNotBetterThan definition to Savage.lean

--- AiSafetyLib/DecisionTheory/Savage.lean
+++ AiSafetyLib/DecisionTheory/Savage.lean
@@ -11,6 +11,15 @@ def completeTransitive {States Consequences : Type}
     (∀ f g : States → Consequences, notBetterThan f g ∨ notBetterThan g f)
     ∧ (∀ f g h : States → Consequences, notBetterThan f g → notBetterThan g h → notBetterThan f h)
 
+def conditionalNotBetterThan {States Consequences : Type}
+  [Nonempty States] [Nonempty Consequences]
+  (notBetterThan : (States → Consequences) → (States → Consequences) → Prop)
+  (event : Set States)
+  (f : States → Consequences)
+  (g : States → Consequences)
+  : Prop :=
+    ∀ h : States → Consequences, notBetterThan (event.piecewise f h) (event.piecewise g h)
+
 def sureThingPrinciple {States Consequences : Type}
   [Nonempty States] [Nonempty Consequences]
   (notBetterThan : (States → Consequences) → (States → Consequences) → Prop)
@@ -18,3 +27,4 @@ def sureThingPrinciple {States Consequences : Type}
     ∀ event : Set States, ∀ f f' g : States → Consequences,
       notBetterThan (event.piecewise f g) (event.piecewise f' g) →
         ∀ g' : States → Consequences, notBetterThan (event.piecewise f g') (event.piecewise f' g')
+
